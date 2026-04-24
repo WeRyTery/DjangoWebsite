93c3adcf2f0bbe58f0bab8c57d21791c2080c875
Ghost <echanakly@gmail.com>
2026-04-24 10:15:13 +0300

    Addon: more informative admin panel
    Made admin panel more informative by removing index display and showing based on item name, price and calories.
    Added search field to search for specific foods.
    Added filter by calories and price where you can see exact amount of items of specific value ( cals. or price )

--- djangoWebsite/restaurantapp/admin.py
+++ djangoWebsite/restaurantapp/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import FoodItems
 
+
+class FoodItemsAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Price', 'Calories')
+    search_fields = ('Name',)
+    list_filter = ('Calories', 'Price')
+
+
 # Register your models here.
-admin.site.register(FoodItems)
+admin.site.register(FoodItems, FoodItemsAdmin)
