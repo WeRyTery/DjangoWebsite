6d0f177aa7037c276ac69b9f7beb445e1a60d1e4
Ghost <133059737+WeRyTery@users.noreply.github.com>
2026-05-11 15:59:33 +0300

    Update urls.py

--- djangoWebsite/djangoWebsite/urls.py
+++ djangoWebsite/djangoWebsite/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse("Main home")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
